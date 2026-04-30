5a9f6ad4353b39ab2f69fdaf5b77565086498e60
aludpasopati-byte <aludpasopati@gmail.com>
2026-04-30 13:30:05 +0700

    else 4-6

--- DoblleLinkedlist .cpp
+++ DoblleLinkedlist .cpp
@@ -240,4 +240,15 @@ int main()
             list.hapus();
             break;
         case '3':
-         
+            list.traverse();
+            break;
+        case '4':
+            list.revtraverse();
+            break;
+        case '5':
+            list.searchData();
+            break;
+        case '6':
+            return 0;
+        default:
+       
