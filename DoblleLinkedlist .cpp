b8c1379198d046a320a801863cda0416e56e0312
aludpasopati-byte <aludpasopati@gmail.com>
2026-04-30 13:24:55 +0700

    void searchdata

--- DoblleLinkedlist .cpp
+++ DoblleLinkedlist .cpp
@@ -181,4 +181,16 @@ public:
         }
     }
 
-    
+    void searchData()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        int rollNo;
+        cout << "\nEnter the roll number to search: ";
+        cin >> rollNo;
+
+        
